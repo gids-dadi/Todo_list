659335a8ff45d118e8e83170ec7dee5d322aa393
gids-dadi <gideons564@gmail.com>
2022-04-29 20:29:11 +0100

    Add method in a new file

--- src/index.js
+++ src/index.js
@@ -1,42 +1,63 @@
 import './style.css';
-import deleteImage from './menu.svg';
+import './add-remove.js';
+import './clear.js';
 
-const todoTasks = [
-  {
-    description: 'wash the dishes',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'complete todo list project',
-    completed: true,
-    index: 2,
-  },
-];
+// import deleteImage from './menu.svg';
+// import { addTodo, deleteTodo, updateDescription, refreshPage } from './todo';
+/*
+// const listform = document.getElementById('todo');
+const todoList = document.getElementById('todo-list');
+const btn = document.querySelector('.btn');
 
-// var elem = document. createElement("hr");
-// elem. setAttribute("width", "100px");
-// document. body. appendChild(elem);
+document
+  .querySelector('.header')
+  .addEventListener('click', () => refreshPage());
 
-function populate() {
-  const listTodo = document.querySelector('.todo-list');
-  todoTasks.forEach((todo) => {
-    const li = document.createElement('li');
-    const div = document.createElement('div');
-    const checkBox = document.createElement('input');
-    const menuIcon = document.createElement('img');
-    const line = document.createElement('hr');
-    const p = document.createElement('p');
-    p.innerHTML = todo.description;
-    checkBox.setAttribute('type', 'checkbox');
-    menuIcon.setAttribute('src', `${deleteImage}`);
-    line.setAttribute('width', '100%');
-    li.appendChild(checkBox);
-    div.appendChild(p);
-    div.appendChild(menuIcon);
-    li.appendChild(div);
-    listTodo.appendChild(line);
-    listTodo.appendChild(li);
+const listform = document.getElementById('todo');
+listform.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    addTodo(event.target.value);
+    // todoList.innerHTML = listform.value;
+  }
+});
+
+const todos = JSON.parse(localStorage.getItem('todos'))
+  ? JSON.parse(localStorage.getItem('todos'))
+  : [];
+
+todos.forEach((todo) => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('list-item');
+  const checkBox = document.createElement('input');
+  const moreIcon = document.createElement('img');
+  const myContentWrapper = document.createElement('div');
+  moreIcon.setAttribute('src', `${deleteImage}`);
+  moreIcon.addEventListener('click', () => {
+    deleteTodo(todo.index);
   });
-}
-populate();
+
+  checkBox.setAttribute('type', 'checkbox');
+  checkBox.checked = todo.completed;
+  // listform.classList.add('todoinput');
+  // listform.disabled = true;
+  // listItem.addEventListener('click', () => {
+  //   listform.disabled = false;
+  // });
+  listform.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      updateDescription(todo.index, e.target.value);
+    }
+  });
+
+  listform.value = todo.description;
+  myContentWrapper.appendChild(checkBox);
+  myContentWrapper.appendChild(todoList);
+  listItem.appendChild(myContentWrapper);
+  listItem.appendChild(moreIcon);
+  todoList.append(listItem);
+});
+
+btn.addEventListener('click', () => {
+  deleteTodo(checkBox.checked);
+});
+*/
