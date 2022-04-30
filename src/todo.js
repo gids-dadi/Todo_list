a394747060d3b5e2e5020de3aeef6b51b870ba60
gids-dadi <gideons564@gmail.com>
2022-04-30 20:43:26 +0100

    Add file to make list interactive

--- src/todo.js
+++ src/todo.js
@@ -1,4 +1,4 @@
-import cleanListener from './todo2.js'
+import cleanListener from './todo2.js';
 let listContainer = [];
 
 class Task {
@@ -52,7 +52,8 @@ export default function populate() {
       checkBox.addEventListener('click', () => {
         checkBox.classList.toggle('fa-square');
         checkBox.classList.toggle('fa-square-check');
-        listContainer[item.index].completed = !listContainer[item.index].completed;
+        listContainer[item.index].completed =
+          !listContainer[item.index].completed;
         localStorage.setItem('container', JSON.stringify(listContainer));
       });
       menuBtn.addEventListener('click', () => {
@@ -63,12 +64,12 @@ export default function populate() {
           menuBtn.classList.add(
             'menu-icon',
             'fa-ellipsis-vertical',
-            `menu-icon${item.index}`,
+            `menu-icon${item.index}`
           );
           menuBtn.classList.remove(
             'delete-icon',
             `delete-icon${item.index}`,
-            'fa-trash-can',
+            'fa-trash-can'
           );
         } else {
           taskDisc.setAttribute('contenteditable', true);
@@ -76,12 +77,12 @@ export default function populate() {
           menuBtn.classList.remove(
             'menu-icon',
             'fa-ellipsis-vertical',
-            `menu-icon${item.index}`,
+            `menu-icon${item.index}`
           );
           menuBtn.classList.add(
             'delete-icon',
             `delete-icon${item.index}`,
-            'fa-trash-can',
+            'fa-trash-can'
           );
         }
       });
@@ -96,12 +97,12 @@ export default function populate() {
         menuBtn.classList.add(
           'menu-icon',
           'fa-ellipsis-vertical',
-          `menu-icon${item.index}`,
+          `menu-icon${item.index}`
         );
         menuBtn.classList.remove(
           'delete-icon',
           `delete-icon${item.index}`,
-          'fa-trash-can',
+          'fa-trash-can'
         );
       });
       // Prevent the Enter key from working while editing the task discriptions.
@@ -170,7 +171,8 @@ export default function populate() {
     itemNo += 1;
   });
   localStorage.setItem('container', JSON.stringify(listContainer));
-  ul.innerHTML += '<li class="clear-text">Clear all completed</li>';
+  ul.innerHTML +=
+    '<li type="button" class="clear-text">Clear all completed</li>';
   listDisplay.appendChild(ul);
   addListener();
   menuListener();
