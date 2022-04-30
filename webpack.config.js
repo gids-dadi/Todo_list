949512ebcf2b12a03abe76adaeb1056e8e82b864
gids-dadi <gideons564@gmail.com>
2022-04-30 12:23:04 +0100

    Fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,6 @@ module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
-    // devServer: {
-    //   static: './dist',
-    // },
   },
 
   plugins: [
@@ -31,10 +28,6 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
       },
     ],
   },
